7fb95e205cab22ef2f60db490c50d363de331a28
Michael Roth <michael@chulado.com>
2017-08-18 22:17:33 -0700

    Implement kids program form

---
+++ seal-team-pt-master/routes/views/contact-kids.js
@@ -0,0 +1,40 @@
+var keystone = require('keystone'),
+	Kids = keystone.list('Kids');
+
+exports = module.exports = function(templateName) {
+	return function(req, res) {
+	
+		var view = new keystone.View(req, res),
+			locals = res.locals;
+		
+		// Set locals
+		locals.section = 'contact';
+		locals.formData = req.body || {};
+		locals.validationErrors = {};
+		locals.enquirySubmitted = false;
+		
+// On POST requests, add the Enquiry item to the database
+		view.on('post', { action: 'contact' }, function(next) {
+			
+			var newKids = new Kids.model(),
+				updater = newCorporate.getUpdateHandler(req);
+			
+			updater.process(req.body, {
+				flashErrors: true,
+				fields: 'name, company, email, phone, message',
+				errorMessage: 'There was a problem submitting your enquiry:'
+			}, function(err) {
+				if (err) {
+					console.log(err.errors);
+					locals.validationErrors = err.errors;
+				} else {
+					locals.enquirySubmitted = true;
+				}
+				next();
+			});
+			
+		});
+		
+		view.render(templateName);
+	};
+};
