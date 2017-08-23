9775545de5de5b92a3228de8e3b9c6c286e14150
Michael Roth <michael@chulado.com>
2017-08-23 11:09:43 -0700

    fix synax err per Conrad

--- seal-team-pt-master/routes/index.js
+++ seal-team-pt-master/routes/index.js
@@ -337,7 +337,7 @@ exports = module.exports = function(app) {
 	app.get("/index.php/component/k2/item/14-john_cnn", function(req, res) { res.redirect(301, "http://sealteampt.com/news"); })	
 	app.get("/index.php/component/k2/item/3-stpt-vcu", function(req, res) { res.redirect(301, "http://sealteampt.com/news"); })	
 	app.get("/index.php/news/22-2011-winter-incentive-program.html", function(req, res) { res.redirect(301, "/"); })	
-	app.get("/index.php/about/instructors/category/15-deane-cheatham", function(req, res) { res.redirect(301, "http://sealteampt.com/join""); })
+	app.get("/index.php/about/instructors/category/15-deane-cheatham", function(req, res) { res.redirect(301, "http://sealteampt.com/join"); })
 
 	// Views
 	app.get('/', routes.views.index);
